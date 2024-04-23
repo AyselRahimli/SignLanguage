2aaaa7025442944dd6d4f87be8fa59a4bce2ad90
AyselRahimli <132754482+AyselRahimli@users.noreply.github.com>
2024-04-23 23:13:22 +0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,49 +1,68 @@
-# prompt: an app which converts normal video with sign language to subtitles
-
-# Import necessary libraries
+import streamlit as st
+from keras.models import load_model
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
+import os
+import mediapipe as mp
+mphands = mp.solutions.hands
+hand = mphands.Hands(max_num_hands=2, min_detection_confidence=0.75)
+mpdraw = mp.solutions.drawing_utils
+st.title("Sign Language Classification")
+st.header("Gesture Model")
+all_classes = os.listdir("C:/Users/harsh/Downloads/ASL")
 
-# Load the trained model
-model = load_model("sign_language_model.h5")
 
-# Define a dictionary to map integer predictions to characters
-word_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+@st.cache(hash_funcs={'tensorflow.python.keras.utils.object_identity.ObjectIdentityDictionary': id}, allow_output_mutation=True)
+def model_upload():
+    model = load_model("C:/Users/harsh/PycharmProjects/Harshvir_S/gestures.h5")
+    print("Loading")
+    return model
 
-# Define a function to preprocess the image
-def preprocess_image(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (64, 64))
-    img = np.array(img) / 255.0
-    return img
 
-# Define a function to predict the sign language
-def predict_sign_language(img):
-    img = preprocess_image(img)
-    prediction = model.predict(np.array([img]))
-    predicted_class = np.argmax(prediction)
-    return word_dict[predicted_class]
+def predict(model, image):
+    img = np.array(image)
+    img = cv2.resize(img, (100, 100))
+    img = img / 255
+    img = np.expand_dims(img, axis=0)
+    prediction = model.predict(img)
+    pred = np.argmax(prediction, axis=-1)
+    return pred, prediction[0][pred[0]]
 
-# Define a function to capture video and generate subtitles
-def generate_subtitles():
-    cap = cv2.VideoCapture(0)
-    subtitles = ""
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        predicted_character = predict_sign_language(frame)
-        subtitles += predicted_character
-        cv2.imshow('Sign Language Recognition', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-    return subtitles
 
-# Generate subtitles
-subtitles = generate_subtitles()
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+if run is False:
+    camera.release()
 
-# Print the generated subtitles
-print(subtitles)
+while run:
+    x_points = []
+    y_points = []
+    _, frame = camera.read()
+    frame = cv2.flip(frame, 1)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hand.process(frame_rgb)
+    if results.multi_hand_landmarks:
+        for landmarks in results.multi_hand_landmarks:
+            for id, ld in enumerate(landmarks.landmark):
+                h, w, channels = frame.shape
+                x_points.append(int(ld.x * w))
+                y_points.append(int(ld.y * h))
+
+            a1 = (int(max(y_points) + 30), int(min(y_points) - 30))
+            a2 = (int(max(x_points) + 30), int(min(x_points) - 30))
+        cv2.rectangle(frame, (a2[1], a1[1]), (a2[0], a1[0]), (0, 255, 0), 3)
+        if len(x_points) == 21 or len(x_points) == 42:
+            target = frame[a1[1]:a1[0], a2[1]:a2[0]]
+
+            if len(target) > 0:
+                m = model_upload()
+                p, num = predict(m, frame)
+                cv2.putText(frame, str(all_classes[p[0]]) + " " + str(100*num), (80, 80), cv2.FONT_ITALIC, 2, (255, 100, 100), 2)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame_rgb)
+
+
+else:
+    st.write('Stopped')
+    camera.release()
